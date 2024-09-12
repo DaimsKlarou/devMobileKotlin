24b89ceda167a5477e887dc8c9a3a0af504ca7d8
Daims Klarou <charles.klarou2023@esatic.edu.ci>
2024-09-12 22:06:09 +0100

    fin du layout de la premiere page de notre application

--- app/src/main/java/com/example/tp1_dev_mobile/MainActivity.kt
+++ app/src/main/java/com/example/tp1_dev_mobile/MainActivity.kt
@@ -17,16 +17,18 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            Tp1_dev_mobileTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
+
+//        setContent {
+//            Tp1_dev_mobileTheme {
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+//                }
+//            }
+//        }
     }
 }
 
