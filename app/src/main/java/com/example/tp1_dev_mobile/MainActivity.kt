ff0b4edf45d8975adaddcdd5353f8ecd04021575
Daims Klarou <charles.klarou2023@esatic.edu.ci>
2024-09-13 03:15:41 +0100

    ajout des fonts dans le projet et modification de la font de textView du message

--- app/src/main/java/com/example/tp1_dev_mobile/MainActivity.kt
+++ app/src/main/java/com/example/tp1_dev_mobile/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.tp1_dev_mobile
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,6 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.res.ResourcesCompat
 import com.example.tp1_dev_mobile.ui.theme.Tp1_dev_mobileTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,6 +23,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        //defintion de variable utiliser dans ma view
+        val btnValider = findViewById<Button>(R.id.btnValider)
+        val btnAnnuler  = findViewById<Button>(R.id.btnAnnuler)
+        val prenoms = findViewById<EditText>(R.id.Prenoms)
+        val name = findViewById<EditText>(R.id.Name)
+
+        btnValider.setOnClickListener{
+            println("Hello World!!")
+        }
 
 //        setContent {
 //            Tp1_dev_mobileTheme {
